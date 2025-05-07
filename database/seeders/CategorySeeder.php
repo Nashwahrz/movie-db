9ba99c68e8464a53e6e6497c11c3e8af489f9023
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2025-05-07 16:01:33 +0700

    seeder and factory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,45 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert(
+            [
+                [
+                    'category_name'=> 'Action',
+                    'description'=> 'Film yang menceritakan perjuangan seekor singa dalam melawan para manusia yang ingin menguasai hutan rimba',
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ],
+
+              [
+                    'category_name'=> 'Comedy',
+                    'description'=> 'Film yang mengambarkan kehidupan pak dadang selama masa penjajahan',
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ],
+
+                [
+                    'category_name'=> 'Drama',
+                    'description'=> 'Film yang mengungkapkan rahasia dodon mengapa dia menghilang dihari pernikahannya',
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ],
+
+                [
+                    'category_name'=> 'Sci-Fi',
+                    'description'=> 'Film dengan latar belakang ilmiah dan teknologi futuristik',
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ],
+
+                [
+                    'category_name'=> 'Romance',
+                    'description'=> 'Film mengambarkan kata kata yang memuakkan',
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ],
+
+
+            ]
+            );
     }
 }
