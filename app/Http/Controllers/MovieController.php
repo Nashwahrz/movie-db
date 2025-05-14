6e0ea0ccabe651244677c0dd917294ffdb53ba43
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2025-05-14 14:37:23 +0700

    Homepage

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+  public function index(){
+    // $movies = Movie::latest() memanggil semua
+     $movies = Movie::latest()->paginate(6); // Ambil 6 film terbaru
+    return view('homepage', compact('movies'));
+   // return view('homepage, ['movie'-> $movies]);
+  }
 }
