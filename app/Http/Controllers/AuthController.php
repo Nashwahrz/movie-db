67be166427cc2ad03be89fb970ffa915f8af1466
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2025-05-28 16:12:28 +0700

    CRUD

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function formLogin(){
-        return view('login');
+        return view('admin.login');
     }
     public function login(Request $request){
          $credentials = $request-> validate([
